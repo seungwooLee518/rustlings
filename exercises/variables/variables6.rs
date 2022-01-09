e4030e1080e93df3f8461df950544dc2b97c4050
SEUNGWOO LEE <seungwoo.lee.518@gmail.com>
2022-01-09 15:50:03 +0900

    fix: variables6.rs
     * Const 변수에는 변수 타입이 명시되어야 한다.

--- exercises/variables/variables6.rs
+++ exercises/variables/variables6.rs
@@ -1,9 +1,9 @@
 // variables6.rs
 // Make me compile! Execute the command `rustlings hint variables6` if you want a hint :)
 
-// I AM NOT DONE
+// Const Variable 과 같은 경우 Type이 명시되어야 한다.
+const NUMBER: i32 = 3;
 
-const NUMBER = 3;
 fn main() {
     println!("Number {}", NUMBER);
 }
