67c031adb78f0583a25e5ec98d74ecf33519f800
SEUNGWOO LEE <seungwoo.lee.518@gmail.com>
2022-01-09 15:47:59 +0900

    fix: variables5.rs
     * to_owned() 함수를 사용하여 해당 변수의 값을 가져올 수 있다.
    
     * https://stackoverflow.com/a/22265792

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -1,11 +1,9 @@
 // variables5.rs
 // Make me compile! Execute the command `rustlings hint variables5` if you want a hint :)
 
-// I AM NOT DONE
-
 fn main() {
     let number = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
-    number = 3;
-    println!("Number plus two is : {}", number + 2);
+    // *.to_owned() 함수를 사용하여 해당 변수에 있는 값을 가져온다.
+    println!("Number plus two is : {}", number.to_owned() + "Two");
 }
